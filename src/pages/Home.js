18f966e7d360dc0cfacd1e050b01b12e0b5f6c31
Harry.Hou <harry.hou@ringcentral.com>
2018-09-05 23:09:42 +0800

    add icon to subject

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import data from '../testdata.json'
-import { Row, Col, BackTop } from 'antd'
+import { Row, Col, BackTop, Icon } from 'antd'
 import TableItem from '../components/Table'
 import DashBoard from '../components/DashBoard'
 console.log(data)
@@ -14,9 +14,9 @@ class HomePage extends Component {
     return (
       <div>
         <BackTop />
-        <h3 className='area_subject'>Dashboard</h3>
+        <h3 className='area_subject'><Icon type='appstore' theme='outlined' />Dashboard</h3>
         <DashBoard {...this.state} />
-        <h3 className='area_subject'>Dashboard</h3>
+        <h3 className='area_subject'><Icon type='appstore' theme='outlined' />Dashboard</h3>
         <div style={{ background: '#fff', padding: 12 }}>
           <TableItem data={testResults} />
         </div>
