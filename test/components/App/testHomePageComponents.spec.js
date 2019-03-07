e68ab62adf918becaa5c3959f5494799eb723c1c
Harry.Hou <harry.hou@ringcentral.com>
2019-03-07 13:50:37 +0800

    :white_check_mark: (Test) add test for app component

---
+++ test/components/App/testHomePageComponents.spec.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import App from '@/app.js';
+
+import { BackTop } from 'antd'
+import HomePage from '@/pages/Home'
+import TableItem from '@/components/Table'
+import DashBoard from '@/components/DashBoard'
+import Information from '@/components/Information'
+
+const wrapper = mount(<App />, {
+  disableLifecycleMethods: false
+});
+const homePage = wrapper.find(HomePage);
+
+
+describe('test home page component info', () => {
+  test('there should have a BackTop in homePage', () => {
+    const homePageBackTop = homePage.find(BackTop);
+    expect(homePageBackTop.length).toBe(1);
+  })
+
+  test('there should have a TableItem info in homePage component', () => {
+    const homePageTableItem = homePage.find(TableItem);
+    expect(homePageTableItem.length).toBe(1);
+  })
+
+  test('there should have a DashBoard info in homePage component', () => {
+    const homePageDashBoard = homePage.find(DashBoard);
+    expect(homePageDashBoard.length).toBe(1);
+  })
+
+  test('there should have a Information info in homePage component', () => {
+    const homePageInformation = homePage.find(Information);
+    expect(homePageInformation.length).toBe(1);
+  })
+})
