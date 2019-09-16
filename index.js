8ad1bf69498a73963d4ba0db6125f18fbd3d821a
AK <52356853+aakti@users.noreply.github.com>
2019-09-16 09:09:41 +0000

    Custom page title and flag to hide default icon (#24)
    * feat: support for custom page title and flag to hide default icon
    
    * test: pageTitle test
    
    * removed unnecessory logs
    
    * typo fix

--- index.js
+++ index.js
@@ -20,14 +20,17 @@ class MyCustomReporter {
     results.config = this._globalConfig
     results.endTime = Date.now()
     results._reporterOptions = this._options
-    const data = JSON.stringify(results)
+    let data = JSON.stringify(results)
     const {
       publicPath = process.cwd(),
       filename = 'jest_html_reporters.html',
+      hideIcon = false,
+      pageTitle = 'Report'
     } = this._options
     fs.existsSync(publicPath) === false && publicPath && mkdirs(publicPath)
     const filePath = path.resolve(publicPath, filename)
     const htmlTemplate = fs.readFileSync(localTemplatePath, 'utf-8')
+    data = JSON.stringify({ testResult: results, config: { pageTitle, hideIcon } });
     const outPutContext = htmlTemplate
       .replace('$resultData', JSON.stringify(data))
     fs.writeFileSync(filePath, outPutContext, 'utf-8')
