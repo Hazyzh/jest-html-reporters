bf0ae985360758a2732f0d27f4b4d186e6520b3e
Harry.Hou <harry.hou@ringcentral.com>
2021-03-26 21:08:34 +0800

    :bug: (fix custom info parse issue)

---
+++ test/globalSetup.js
@@ -0,0 +1,7 @@
+// setup.js
+module.exports = async () => {
+  // ...
+  // Set reference to mongod in order to close the server during teardown.
+  const mycustInfo = { Environment: "dev", Browser: "chrome" };
+  process.env.JEST_HTML_REPORTERS_CUSTOM_INFOS = JSON.stringify(mycustInfo);
+};
