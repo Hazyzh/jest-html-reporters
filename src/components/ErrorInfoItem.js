75a24c97582c0b1c693e94a9348b5827967fcda2
Harry Hou <harry.hou@ringcentral.com>
2021-12-29 01:02:07 +0800

    feat: 🎸 video attach display support

--- src/components/ErrorInfoItem.js
+++ src/components/ErrorInfoItem.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import Convert from 'ansi-to-html'
-import escapeHtml from 'escape-html'
+import React from 'react';
+
+import Convert from 'ansi-to-html';
+import escapeHtml from 'escape-html';
 
 const convert = new Convert({
   fg: '#595959'
-})
+});
 
 const createMarkup = (text) => ({
   __html: convert.toHtml(escapeHtml(text))
-})
+});
 
 const ErrorInfoItem = ({ data }) => {
-  if (!data) return null
-  return <pre dangerouslySetInnerHTML={createMarkup(data)} />
-}
+  if (!data) return null;
+  return <pre dangerouslySetInnerHTML={createMarkup(data)} />;
+};
 
-export default ErrorInfoItem
+export default ErrorInfoItem;
