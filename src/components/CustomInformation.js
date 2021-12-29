411256907f79e47d43f29f6bfffa54c5aebf4cb1
Harry Hou <harry.hou@ringcentral.com>
2021-12-29 23:14:59 +0800

    feat: 🎸 add html support for custom info section

--- src/components/CustomInformation.js
+++ src/components/CustomInformation.js
@@ -1,10 +1,15 @@
-import React from "react";
-import { Descriptions } from "antd";
+import React from 'react';
+
+import { Descriptions } from 'antd';
+
+const createMarkup = (text) => ({
+  __html: text
+});
 
 export default ({ customInfos }) => (
   <Descriptions bordered>
     {customInfos.map(({ title, value }) => (
-      <Descriptions.Item label={title}>{value}</Descriptions.Item>
+      <Descriptions.Item key={title} label={title}><p dangerouslySetInnerHTML={createMarkup(value)} /></Descriptions.Item>
     ))}
   </Descriptions>
 );
