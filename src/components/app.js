fea12ac884b5470342254a6fd7de7085a44515d8
Harry Hou <harry.hou@ringcentral.com>
2022-03-04 16:41:52 +0800

    style: 💄 modify log message style

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,31 @@
-import React, { Component } from "react";
+import '../styles/app.less';
+import '../styles/index.less';
+import '../styles/dashBoard.less';
+import '../styles/information.less';
+import '../styles/footer.less';
+import '../styles/errorButton.less';
+
+import React, { Component } from 'react';
+
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Layout, Avatar } from "antd";
-import HomePage from "../pages/HomePage";
-import FooterInfo from "./FooterInfo";
-import "../styles/app.less";
-import "../styles/index.less";
-import "../styles/dashBoard.less";
-import "../styles/information.less";
-import "../styles/footer.less";
-import { GithubFilled } from "@ant-design/icons";
+import {
+  Avatar,
+  Layout,
+} from 'antd';
+
+import { GithubFilled } from '@ant-design/icons';
 
 // context
-import { Provider } from "../contexts/expand";
+import { Provider } from '../contexts/expand';
+import HomePage from '../pages/HomePage';
+import FooterInfo from './FooterInfo';
 
 const getInitData = ({ testResults = [] }) =>
   testResults.reduce((pre, item) => {
     pre[item.testFilePath] = false;
     return pre;
   }, {});
-const defaultTitle = "Report";
+const defaultTitle = 'Report';
 const { Header, Content, Footer } = Layout;
 class App extends Component {
   constructor(props) {
@@ -39,33 +46,33 @@ class App extends Component {
     const { logoImg } = data._reporterOptions;
     const IconComp = hideIcon ? null : (
       <a
-        target="_blank"
-        className="icon"
-        href="https://github.com/Hazyzh/jest-html-reporters"
+        target='_blank'
+        className='icon'
+        href='https://github.com/Hazyzh/jest-html-reporters'
       >
         <GithubFilled />
       </a>
     );
     return (
       <Provider value={this.state}>
-        <Layout className="layout">
+        <Layout className='layout'>
           {(logoImg || pageTitle || IconComp) &&
-              <Header className='header'>
-                {logoImg && (
-                  <Avatar
-                    src={logoImg}
-                    shape='square'
-                    size='large'
-                    style={{ marginRight: '1em' }}
-                  />
-                )}
-                <span data-testid='page-title'>{pageTitle}</span>
-                {IconComp}
-              </Header>}
-          <Content style={{ padding: "0 50px" }}>
+            <Header className='header'>
+              {logoImg && (
+                <Avatar
+                  src={logoImg}
+                  shape='square'
+                  size='large'
+                  style={{ marginRight: '1em' }}
+                />
+              )}
+              <span data-testid='page-title'>{pageTitle}</span>
+              {IconComp}
+            </Header>}
+          <Content style={{ padding: '0 50px' }}>
             <HomePage realData={data} />
           </Content>
-          <Footer style={{ marginTop: "50px", background: "#000" }}>
+          <Footer style={{ marginTop: '50px', background: '#000' }}>
             <FooterInfo />
           </Footer>
         </Layout>
