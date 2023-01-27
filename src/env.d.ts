50f036a7320aed3bd32a90002ed446163f2d5121
harry.hou <harry.hou@ringcentral.com>
2023-01-27 16:11:34 +0800

    add

---
+++ src/env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+
+interface Window {
+  realData: any;
+}
+
+declare module '*.svg' {
+  import * as React from 'react'
+
+  const ReactComponent: React.FunctionComponent<
+    React.ComponentProps<'svg'> & { title?: string }
+  >
+  export default  ReactComponent;
+}
